10d55d9c2a0daa765fad6739522018f4f43be96e
161250008 <161250008@smail.nju.edu.cn>
2019-01-24 12:23:16 +0800

    pom.xml : add <package>war</package>.

--- src/main/java/edu/nju/SSDAAssignmentThree/SSDAAssignmentThreeApplication.java
+++ src/main/java/edu/nju/SSDAAssignmentThree/SSDAAssignmentThreeApplication.java
@@ -2,10 +2,8 @@ package edu.nju.SSDAAssignmentThree;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+
 @SpringBootApplication
 public class SSDAAssignmentThreeApplication {
 
@@ -13,10 +11,5 @@ public class SSDAAssignmentThreeApplication {
 		SpringApplication.run(SSDAAssignmentThreeApplication.class, args);
 	}
 
-
-	@RequestMapping(value = "/",produces = "text/plain;charset=UTF-8")
-	String index(){
-		return "helloWorld";
-	}
 }
 
