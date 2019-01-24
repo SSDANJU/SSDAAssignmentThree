673201a0369b99751777ced32ad2d7081854bac4
161250008 <161250008@smail.nju.edu.cn>
2019-01-24 21:28:23 +0800

    finish UI of component 2&4.

--- src/main/java/edu/nju/SSDAAssignmentThree/controller/IndexController.java
+++ src/main/java/edu/nju/SSDAAssignmentThree/controller/IndexController.java
@@ -6,8 +6,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping("/index")
-    public String hello(){
-        return "helloWorld";
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @RequestMapping("/bookInfoEdit")
+    public String bookInfoEdit(){
+        return "bookInfoEdit";
+    }
+
+    @RequestMapping("/bookInfoEditDetail")
+    public String bookInfoEditDetail(){
+        return "bookInfoEditDetail";
+    }
+
+    @RequestMapping("/userInfoEdit")
+    public String userInfoEdit(){
+        return "userInfoEdit";
+    }
+
+    @RequestMapping("/userInfoEditDetail")
+    public String userInfoEditDetail(){
+        return "userInfoEditDetail";
+    }
+
+    @RequestMapping("/personalInfoEdit")
+    public String personalInfoEdit(){
+        return "personalInfoEdit";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
     }
 }
